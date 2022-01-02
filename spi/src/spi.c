2bfd3540d43af040d3b5da7f13c3f70ead71a396
Alberto Tejada <alberto.tejada.molina@outlook.es>
2022-01-02 18:25:35 +0100

    Integration test for SPI updated
    * spi_test updated and executed
    * Corresponding CMakeLists file updated to include the GPIO library
    * In the SPI source code, a commented line to set SPI speed to fosc/8
      added. This is required to execute the integration test
    * CNT integration test file added as an empty file to fix CMake build
      error

--- spi/src/spi.c
+++ spi/src/spi.c
@@ -6,12 +6,17 @@ void spi_init(void) {
 	SPI_CLK_DIR |= (OUTPUT << SPI_CLK);
 	SPI_MOSI_DIR |= (OUTPUT << SPI_MOSI);
 	SPI_MISO_DIR &= ~(OUTPUT << SPI_MISO);
+	// Set SCK and MOSI in LOW state:
+	SPI_CLK_PORT &= ~(1 << SPI_CLK);
+	SPI_MOSI_PORT &= ~(1 << SPI_MOSI);
 	// Set the SPI configuration:
 	SPI_CTRL = (SPI_ENABLE +\
 							MSBFIRST +\
 							SPI_MASTER +\
 							SPI_MODE0);
 	SPI_STAT = SPI_SPEED;
+	// For IT, set SPR0 bit to 1, so SPI speed is fosc/8
+	/* SPI_CTRL |= (1); */
 }
 
 uint8_t spi_transfer(uint8_t data) {
--- spi/test/spi_test.c
+++ spi/test/spi_test.c
@@ -6,18 +6,55 @@
  */
 
 #include "spi.h"
+#include "gpio.h"
 #include <util/delay.h>
 
+/**
+ * \brief SPI Master on target test:
+ * This is a simple test to verify the behaviour of the SPI
+ * communication.
+ * In order to execute this test, an aditional uC is
+ * required as SPI slave with the following set up:
+ * 1) Connect master-slave MISO (PB4), MOSI(PB3), SPI Clock
+ *    (PB5) and CAN_DRV_CS (PB2).
+ * 2) Assuming an Arduino board in the slave side, flash the
+ *    code for the SPI slve of this post:
+ *    https://forum.arduino.cc/t/arduino-as-spi-slave/52206/2
+ * 3) As the test is expecting slave-master communication,
+ *    modify the slave code to load a character in the
+ *    SPDR register.
+ * 4) Verify that both uC share a common GND and Vss to
+ *    avoid damage them.
+ * 5) Connect the UART to the slave and verify that the
+ *    message `Hello World! X` is printed, where `X` is the
+ *    character that sent by the slave.
+ * 
+ * \note
+ * If the slave board is an ATmega328p, the SPI Clock speed
+ * shall be set to fosc/8 instead of fosc/2
+ */
 int main(void) {
 
-	uint8_t output = 0;
+	char test[] = "Hello World! 0\n";
+	uint8_t idx = 0;
+	uint8_t len = 15;
+	uint8_t input; 
+	gpio_init();
+	can_drv_deselect();
 	spi_init();
 	_delay_ms(1000);
 
 	while(1)
 	{
-		spi_transfer(output);
-		output++;
+		can_drv_select();
+		input = spi_transfer(test[idx]);
+		can_drv_deselect();
+		idx++;
+		if(idx == len)
+		{
+			idx = 0;
+			test[13] = input;
+		}
 		_delay_ms(200);
 	}
 	return 0;
