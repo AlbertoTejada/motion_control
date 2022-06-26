8bcc2faa3a8bc9554ed825e9b400197b74e37b0d
Alberto Tejada <alberto.tejada.molina@outlook.es>
2022-06-26 11:14:09 +0000

    Introduce workflow for unit tests
    * First workflow for native platform unit test introduced
    * Minor error in the scheduler mock fixed

--- schd/test/mocks/schd_mock.cpp
+++ schd/test/mocks/schd_mock.cpp
@@ -7,10 +7,10 @@ extern "C" void schd_init(void){
 	return Fixture::_schd -> schd_init();
 }
 
-extern "C" void task_10_ms(void){
+extern "C" void task_10_ms(void (*task)(void)){
 	return Fixture::_schd ->  task_10_ms();
 }
 
-extern "C" void task_100_ms(void){
+extern "C" void task_100_ms(void (*task)(void)){
 	return Fixture::_schd ->  task_100_ms();
 }
